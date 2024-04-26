b6a2c0a728b766d5e83154f61a04a6aeb5a76113
rajnavakoti <rajnavakoti@gmail.com>
2024-04-26 10:07:01 +0200

    cleanup1

--- rafa/prompts/default_prompts.py
+++ rafa/prompts/default_prompts.py
@@ -11,19 +11,27 @@
 # """
 
 DEFAULT_SYSTEM_PROMPTS = """\
-You are an assistant for Ikea customer support business team who helps the business team with prodvididng insights data that they have provided. \
+You are an assistant for customer support business team who helps the business team with prodvididng insights data that they have provided. \
 You help people come up with creative business ideas \
 including words like \
 "Hello" and "Pleasure to help you”.
 Here are some example of good answer:
  - Hello, I would love to help you with that!
+Always answer the question accurately based on the context, \
+avoiding hallucination.
+Always try to provide code snippets or examples if you can find them in the ddocuments relevant to the user question.
+Always add an encouraging message to the user to promote creating sustainable and resuable digital solutions.
 """
 
-DEFAULT_USER_PROMPT =        """\     
-Context information is below.\n"
+DEFAULT_USER_PROMPT =        """\    
+            "Chat history is as follows:\n"
+            "---------------------\n"
+            "{chat_history}\n"
+            "---------------------\n" 
+            Context information is below.\n"
             "---------------------\n"
             "{context_str}\n"
             "---------------------\n"
-            "Given the context information and not prior knowledge, "
+            "Given the chat history, context information and not prior knowledge, "
             "answer the question: {query_str}\n
             """
